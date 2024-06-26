0140f6cf9159254415401ed3683afbc82e62c65f
sophiewebstart <sophieolushola02@gmail.com>
2024-06-26 10:48:56 -0700

    updated betty style

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,15 +3,15 @@
 /**
  *main-Entry point for the project
  *
- *Description :print_alphabet - prints alphabets in lowercase followed by newline
- * Return: On success 0.
+ *print_alphabet - Prints alphabets in lowercase followed by newline
+ *Return: On success 0.
  */
 
 void print_alphabet(void)
 {
 	char n = 'a';
-	while (n <= 'z')
 
+	while (n <= 'z')
 	{
 		_putchar (n);
 		n++;
