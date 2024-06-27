b13c3d6a32682af38bd32562ba592818ae9fc303
sophiewebstart <sophieolushola02@gmail.com>
2024-06-26 21:15:22 -0700

    using islower function

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,14 +4,16 @@
  *main-Entry point for the project
  *
  *Description: Writes a program that checks for lowercase character
+ *using _islower function
  *
+ * @c: Character to check
  * Return: On success 1 if c is lowercase, 0 otherwise
  */
 
 int _islower(int c)
 {
-	if(c >= 'a' && c <= 'z')
-		return 1;
+	if (c >= 'a' && c <= 'z')
+		return (1);
 	else
-		return 0;
+		return (0);
 }
