aee9d0a11f98188725bd1815bd51fabac1f4cbcc
Sophie <sophieolushola02@gmail.com>
2024-07-18 02:53:04 +0100

    using argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - prints the name of a program
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Description - writes a program that prints its name 
+  * followed by a new line
+  *
+  * Returns: 0 on success
+  */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	(void)argc;
+
+	while( argv[0][i] !='\0')
+	{
+		_putchar (argv[0][i]);
+		i++;
+	}
+	_putchar ('\n');
+
+	return (0);
+}
