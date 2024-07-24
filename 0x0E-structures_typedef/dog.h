e420abb1adc8748c35db552c2c1029f17cc6eb10
Sophie <sophieolushola02@gmail.com>
2024-07-24 03:13:47 +0100

    using structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+struct dog *my_dog(char *name, float age, char*owner)
+{
+	struct dog *ptr;
+
+	ptr = malloc(sizeof(struct dog));
+	if(ptr == NULL)
+	{
+		return (NULL);
+		ptr->name = name;
+		ptr->age = age;
+		ptr->owner = owner;
+	}
