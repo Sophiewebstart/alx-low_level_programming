755573b15d2d4cd71b5cdfe9064581e6de90e3ab
Sophie <sophieolushola02@gmail.com>
2024-08-07 02:15:02 +0100

    update for free list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,15 +1,15 @@
 #include "lists.h"
 
 /**
-  * free_listint2 - this function frees a list
+  * free_listint2 - this function frees a list of nodes
   * and sets the head to NULL
-  * @head: pointer to the head of the list
+  * @head: pointer to the first element of the list
   */
 void free_listint2(listint_t **head)
 {
 	listint_t *t;
 
-	if (head == NULL || *head != NULL)
+	if (head == NULL)
 		return;
 	while ((*head)->next != NULL)
 	{
